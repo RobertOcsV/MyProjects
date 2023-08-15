2ca75b159036790026b109fd909b93de7bb82439
RobertOcsV <robertoliveiracor@hotmail.com>
2023-08-15 11:51:06 -0300

    initial commit

--- logicar/frontend/logicarf/src/app/app.module.ts
+++ logicar/frontend/logicarf/src/app/app.module.ts
@@ -1,16 +1,26 @@
+
 import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
+    HeaderComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
