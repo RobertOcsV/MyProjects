86e98ab830ab022665cdf040f59939fb48e26d64
RobertOcsV <robertoliveiracor@hotmail.com>
2023-12-20 16:07:19 -0300

    config: ajust de ambiente e configuração

---
+++ crud-angular-spring/crud-angular/src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TesteComponent } from './teste/teste.component';
+
+@NgModule({
+  declarations: [ TesteComponent ],
+  imports: [BrowserModule],
+  providers: [  ],
+})
+export class AppModule { }
