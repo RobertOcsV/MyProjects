0ea324746b14b0548af01f13b5ec1d3088a5dd90
RobertOcsV <robertoliveiracor@hotmail.com>
2023-12-22 23:03:22 -0300

    feat: new app component

--- crud-angular-spring/crud-angular/src/app/courses/courses.module.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CoursesComponent],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    MatToolbarModule
   ]
 })
 export class CoursesModule { }
