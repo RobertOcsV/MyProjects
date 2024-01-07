88613afaab54ff39790ca9a7ffdb95a44a7ea116
RobertOcsV <robertoliveiracor@hotmail.com>
2024-01-07 13:53:13 -0300

    feat: pipe category icon courses one way binding

--- crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -5,6 +5,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -16,7 +17,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
 
 
   ]
