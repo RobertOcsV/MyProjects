8f3134da37b2d38fddbc65c4751adb1c45b02d40
RobertOcsV <robertoliveiracor@hotmail.com>
2024-01-11 11:39:48 -0300

    feat: list of courses method initDataBase

---
+++ crud-angular-spring/spring-crud/src/main/java/com/robert/SpringCrudApplication.java
@@ -0,0 +1,30 @@
+package com.robert;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.robert.model.Course;
+import com.robert.repository.CourseRepository;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+			courseRepository.save(c);
+		};
+	}
+}
---
+++ crud-angular-spring/spring-crud/src/main/java/com/robert/controller/CourseController.java
@@ -0,0 +1,27 @@
+package com.robert.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.robert.model.Course;
+import com.robert.repository.CourseRepository;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+        
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
