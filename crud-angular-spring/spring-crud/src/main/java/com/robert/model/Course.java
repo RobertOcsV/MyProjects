2805ba32a0040881de2fa5e5ade3f087f6630f96
RobertOcsV <robertoliveiracor@hotmail.com>
2024-01-13 21:29:17 -0300

    feat:  config database aplication props

--- crud-angular-spring/spring-crud/src/main/java/com/robert/model/Course.java
+++ crud-angular-spring/spring-crud/src/main/java/com/robert/model/Course.java
@@ -14,8 +14,10 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
     @Column(length = 200, nullable = false)
     private String name;
+
     @Column(length = 10, nullable = false)
     private String category;
 }
