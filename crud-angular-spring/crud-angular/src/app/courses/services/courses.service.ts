1205a2ac44d79d5f4222904a4a0a58afdda29caf
RobertOcsV <robertoliveiracor@hotmail.com>
2024-01-17 16:56:47 -0300

    feat: new extension color and api url changed

--- crud-angular-spring/crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular-spring/crud-angular/src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import { delay, first, tap } from 'rxjs';
 })
 export class CoursesService {
 
-  private readonly API = "assets/courses.json";
+  private readonly API = "http://localhost:8080/api/courses";
 
   constructor(private httpCliente: HttpClient) { }
 
