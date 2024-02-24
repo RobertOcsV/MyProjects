63f3da071fb7cd1ef37621cf877a1c3faaa40f88
RobertOcsV <robertoliveiracor@hotmail.com>
2024-02-24 17:53:54 -0300

    feat: aula 41 Padrão DTO

---
+++ crud-angular-spring/spring-crud/src/main/java/com/robert/dto/CourseDTO.java
@@ -0,0 +1,17 @@
+package com.robert.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record CourseDTO(
+        @JsonProperty("_id") Long id,
+        @NotBlank @NotNull @Length(min = 5, max = 10) String nome,
+        @NotNull @Length (max = 10) @Pattern(regexp = "Back-end|Front-end") String categoria
+       ) {
+
+}
