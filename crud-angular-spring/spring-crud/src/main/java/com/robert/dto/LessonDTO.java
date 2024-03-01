31df4d4ce05d2e9eda693be84a29ffb4ff6771fd
RobertOcsV <robertoliveiracor@hotmail.com>
2024-03-01 05:50:49 -0300

    feat: Aula 56  Erros de Validação da Api e remoção lombok - projeto pequeno

--- crud-angular-spring/spring-crud/src/main/java/com/robert/dto/LessonDTO.java
+++ crud-angular-spring/spring-crud/src/main/java/com/robert/dto/LessonDTO.java
@@ -1,6 +1,11 @@
 package com.robert.dto;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record LessonDTO(
         Long id,
-        String name,
-        String youtubeUrl) {}
+        @NotNull @NotBlank @Length(min = 5, max = 100) String name,
+        @NotNull @NotBlank @Length(min = 5, max = 11)  String youtubeUrl) {}
