fda6120598a00f59e7cb04659e9f1442d346f7c2
RobertOcsV <robertoliveiracor@hotmail.com>
2024-03-01 17:41:59 -0300

    feat: aula 57 - vlidações Enum com Java Bean

--- crud-angular-spring/spring-crud/src/main/java/com/robert/dto/CourseDTO.java
+++ crud-angular-spring/spring-crud/src/main/java/com/robert/dto/CourseDTO.java
@@ -5,17 +5,18 @@ import java.util.List;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.robert.enums.Category;
+import com.robert.enums.Validation.ValueOfEnum;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotBlank @NotNull @Length(min = 5, max = 100) String name,
-        @NotNull @Length (max = 10) @Pattern(regexp = "Back-end|Front-end") String category, 
+        @NotNull @Length (max = 10) @ValueOfEnum(enumClass = Category.class) String category,  //@Pattern(regexp = "Back-end|Front-end")
         @NotNull @NotEmpty @Valid List<LessonDTO> lessons
        ) {
         
