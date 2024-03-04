ff8fa99d50b0c1173c4cb05dab7198ecf36c8f74
RobertOcsV <robertoliveiracor@hotmail.com>
2024-03-03 21:09:01 -0300

    feat: aula 62 parte 3 remoção app module bootstrap to main

--- crud-angular-spring/crud-angular/src/app/app.component.ts
+++ crud-angular-spring/crud-angular/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { Router,  } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    standalone: true,
+    imports: [MatToolbarModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'crud-angular';
--- crud-angular-spring/crud-angular/src/main.ts
+++ crud-angular-spring/crud-angular/src/main.ts
@@ -1,4 +1,18 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 
-  platformBrowserDynamic().bootstrapModule(AppModule);
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+  bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(CommonModule, BrowserModule, AppRoutingModule, MatToolbarModule),
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+});
