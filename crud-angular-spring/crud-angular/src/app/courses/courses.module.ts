9ea16c26f7d10610c18c77b586a71698f47e9179
RobertOcsV <robertoliveiracor@hotmail.com>
2024-03-03 21:07:21 -0300

    feat: aula 62 parte 2  remoção dos módulos //   ng generate @angular/core:standalone

--- crud-angular-spring/crud-angular/src/app/courses/courses.module.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses.module.ts
@@ -3,20 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './container/courses/courses.component';
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { SharedModule } from '../shared/shared.module';
+
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { CourseFormComponent } from './container/course-form/course-form.component';
 import { CoursesListComponent } from './components/courses-list/courses-list.component';
 
 @NgModule({
     imports: [
-        CommonModule,
-        CoursesRoutingModule,
-        AppMaterialModule,
-        SharedModule,
-        ReactiveFormsModule,
-        CoursesComponent, CourseFormComponent, CoursesListComponent
-    ]
+    CommonModule,
+    CoursesRoutingModule,
+    ReactiveFormsModule,
+    CoursesComponent, CourseFormComponent, CoursesListComponent
+]
 })
 export class CoursesModule { }
